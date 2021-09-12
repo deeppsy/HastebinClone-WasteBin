790f43ad05db01305ded87b6bc387ebdefab0183
Dipo <adanikindipo@gmail.com>
2021-09-12 10:13:00 +0100

    set up basic ejs view engine

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.render("code-display");
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
