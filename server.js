6259ddea1cfd32098eec745b42b3fc443ce289e2
Dipo <adanikindipo@gmail.com>
2021-09-12 19:45:48 +0100

    add start script in package.json and use default port from heroku

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ app.get("/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
